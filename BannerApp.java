efb3e86873de29431b37931ad33739d7ac5c7d14
AkshatWalvekar <akshatwalvekar@gmail.com>
2026-02-18 16:16:11 +0530

    Used String.join() function

--- BannerApp.java
+++ BannerApp.java
@@ -1,23 +1,22 @@
 
 /*
 @author Developer
-@version 2
+@version 3
 */
 
 
 
 
-
-class Message
+class Banner
 {
 	public static void main(String[]args)
 	{
-		System.out.println("   ***   " + " " + "   ***   " + " " + "*********" + " " + "   *****");
-		System.out.println(" **   ** " + " " + " **   ** " + " " + "**      *" + " " + " **     ");
-		System.out.println("**     **" + " " + "**     **" + " " + "**      *" + " " + "**      ");
-		System.out.println("**     **" + " " + "**     **" + " " + "*********" + " " + "  ***** ");
-		System.out.println("**     **" + " " + "**     **" + " " + "**       " + " " + "      **");
-		System.out.println(" **   ** " + " " + " **   ** " + " " + "**       " + " " + " **   **");
-		System.out.println("   ***   " + " " + "   ***   " + " " + "**       " + " " + "  ***** ");
+		System.out.println(String.join(" ", "   ***   ", "   ***   ", "*********", "   *****"));
+		System.out.println(String.join(" ", " **   ** ", " **   ** ", "**     **", " **     "));
+		System.out.println(String.join(" ", "**     **", "**     **", "**     **", "**      "));
+		System.out.println(String.join(" ", "**     **", "**     **", "*********", "  ***** "));
+		System.out.println(String.join(" ", "**     **", "**     **", "**       ", "      **"));
+		System.out.println(String.join(" ", " **   ** ", " **   ** ", "**       ", " **   **"));
+		System.out.println(String.join(" ", "   ***   ", "   ***   ", "**       ", "  ***** "));
 	}
 }
