272ca0384f65a89f2aaf29857dad338ce0d0b6de
AkshatWalvekar <akshatwalvekar@gmail.com>
2026-02-18 17:14:58 +0530

    Printed BannerApp using Methods

--- BannerApp.java
+++ BannerApp.java
@@ -9,21 +9,49 @@
 
 class BannerApp
 {
-	public static void main(String[]args)
-	{
-		String line[] = {
-	
-		String.join(" ", "   ***   ", "   ***   ", "*********", "   *****"),
-		String.join(" ", " **   ** ", " **   ** ", "**     **", " **     "),
-		String.join(" ", "**     **", "**     **", "**     **", "**      "),
-		String.join(" ", "**     **", "**     **", "*********", "  ***** "),
-		String.join(" ", "**     **", "**     **", "**       ", "      **"),
-		String.join(" ", " **   ** ", " **   ** ", "**       ", " **   **"),
-		String.join(" ", "   ***   ", "   ***   ", "**       ", "  ***** ")
+	public static String[] getOPattern(){
+		return new String[]{
+			"   ***   ",
+			" **   ** ",
+			"**     **",
+			"**     **",
+			"**     **",
+			" **   ** ",
+			"   ***   "
 		};
-		
-		for(String str: arr){
-		System.out.println(str);
-		}
 	}
+	
+	public static String[] getPPattern(){
+		return new String[]{
+			"*********",
+			"**     **",
+			"**     **",
+			"*********",
+			"**       ",
+			"**       ",
+			"**       "
+		};
+	}
+	
+	public static String[] getSPattern(){
+		return new String[]{
+			"   *****",
+			" **     ",
+			"**      ",
+			"  ***** ",
+			"      **",
+			" **   **",
+			"  ***** "
+		};
+	}
+	
+	public static void main(String[] args){
+		String[] OPattern = getOPattern();
+		String[] PPattern = getPPattern();
+		String[] SPattern = getSPattern();
+		
+	for(int i = 0; i<OPattern.length;i++){
+		System.out.println(OPattern[i] + " " + OPattern[i] + " " + PPattern[i] + " " + SPattern[i]);
+	}
+}
 }
