d22a303cce089c43a8d3a49a962f04e30a58addd
AkshatWalvekar <akshatwalvekar@gmail.com>
2026-02-20 10:39:49 +0530

    Wrote code using Inner class

--- BannerApp.java
+++ BannerApp.java
@@ -1,57 +1,109 @@
 
-/*
-@author Developer
-@version 5.0
-*/
 
 
+import java.util.Scanner;
 
-
-class BannerApp
-{
-	public static String[] getOPattern(){
-		return new String[]{
-			"   ***   ",
-			" **   ** ",
-			"**     **",
-			"**     **",
-			"**     **",
-			" **   ** ",
-			"   ***   "
-		};
+class OOPSBannerApp{
+	static class CharacterPatternMap{
+		String[] pattern;
+		Character character;
+		CharacterPatternMap(Character character,String[] pattern){
+			this.character=character;
+			this.pattern=pattern;
+		
+		}
+		
 	}
-	
-	public static String[] getPPattern(){
-		return new String[]{
-			"*********",
-			"**     **",
-			"**     **",
-			"*********",
-			"**       ",
-			"**       ",
-			"**       "
+    public static CharacterPatternMap[] createCharacterPatternMaps(){
+		
+        String getpatternO[]={
+            "  ***  ",
+            " ** ** ",
+            "**   **",
+            "**   **",
+            "**   **",
+            "**   **",
+            "**   **",
+            " ** ** ",
+            "  ***  "	
+        };
+        String getpatternP[]={
+			" ****** ",
+            " **   **",
+            " **   **",
+            " **   **",
+            " ****** ",
+            " **     ",
+            " **     ",
+            " **     ",
+            " **     "
+        };
+        String getpatternS[]={
+            " ***** ",
+            " **    ",
+            " **    ",
+            " **    ",
+            "  ***  ",
+            "    ** ",
+            "    ** ",
+            "    ** ",
+            " ***** "	
+        };
+		String patternspace[]={
+			" ",
+			" ",
+			" ",
+			" ",
+			" ",
+			" ",
+			" ",
+			" ",
+			" "
 		};
+		CharacterPatternMap a=new CharacterPatternMap('o',getpatternO);
+		CharacterPatternMap b=new CharacterPatternMap('p',getpatternP);
+		CharacterPatternMap c=new CharacterPatternMap('s',getpatternS);
+		CharacterPatternMap d=new CharacterPatternMap(' ',patternspace);
+		return new CharacterPatternMap[]{a,b,c,d};
+			
+        		
 	}
-	
-	public static String[] getSPattern(){
-		return new String[]{
-			"   *****",
-			" **     ",
-			"**      ",
-			"  ***** ",
-			"      **",
-			" **   **",
-			"  ***** "
-		};
-	}
+    public static String[] getCharacterPattern(char ch,CharacterPatternMap[] charMaps){
+		for(CharacterPatternMap charac:charMaps){
+            if(charac.character.equals(ch)){
+				String getp[]=charac.pattern;
+				return getp;
+			}	
+		}
+		return new String[]{" "};
+    }	
+    public static void printMessage(String message,CharacterPatternMap[] charMaps){
+		char ch[]=message.toCharArray();
+		StringBuilder[] lines=new StringBuilder[9];
+		for(int i=0;i<lines.length;i++){
+			lines[i]=new StringBuilder();
+		}
+		
+		for(char chh:ch){
+			String newpat[]=getCharacterPattern(chh,charMaps);
+			
+		for(int i=0;i<9;i++){
+		lines[i].append(newpat[i]).append(" ");
+			}
+		}
+		
+		for(StringBuilder l:lines){
+			System.out.println(l.toString());
+		}
+}
 	
 	public static void main(String[] args){
-		String[] OPattern = getOPattern();
-		String[] PPattern = getPPattern();
-		String[] SPattern = getSPattern();
-		
-	for(int i = 0; i<OPattern.length;i++){
-		System.out.println(OPattern[i] + " " + OPattern[i] + " " + PPattern[i] + " " + SPattern[i]);
-	}
-}
-}
+		CharacterPatternMap[] mp=createCharacterPatternMaps();
+		System.out.println("Enter a string in lowercase:");
+		Scanner sc=new Scanner(System.in);
+		String s=sc.nextLine();
+		printMessage(s,mp);
+    }
+}	
+	
+    	
